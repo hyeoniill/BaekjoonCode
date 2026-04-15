1332106cca7ca3c918260ebf4ef54abc5342424e
Hyeoniill <52920187+hyeoniill@users.noreply.github.com>
2026-04-15 18:59:12 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,15 +1,8 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int[] arr = new int[10];
+        int answer = 45;
         for(int i=0; i<numbers.length; i++) {
-            arr[numbers[i]] = -1;
-        }
-        
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] != -1) {
-                answer += i;
-            }
+            answer -= numbers[i];
         }
         return answer;
     }
