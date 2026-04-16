7703a6e90c35c559e0cd620bb5a5e6a50405b455
Hyeoniill <52920187+hyeoniill@users.noreply.github.com>
2026-04-16 15:58:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 48.65 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -4,7 +4,11 @@ class Solution {
     public String solution(String number, int k) {
         Deque<Character> deque = new ArrayDeque<>();
         
+        // number를 char하나씩 분해해서 계산
         for(char c : number.toCharArray()) {
+            // deque가 비어있으면 무조건 추가
+            // k개의 숫자를 제거 해야하기 때문에 뺄때마다 k--를 해주어야 함
+            // deque의 마지막 자릿수가 현재 c보다 작다면 deque의 뒤부터 숫자를 하나씩 제거
             while(!deque.isEmpty() && k > 0 && deque.peekLast() < c) {
                 deque.pollLast();
                 k--;
@@ -12,7 +16,8 @@ class Solution {
             deque.addLast(c);
         }
         
-        // 아직 제거 안 했으면 뒤에서 제거
+        
+        // 아직 k개 만큼 제거 안 했으면 뒤에서 제거
         while(k > 0) {
             deque.pollLast();
             k--;
@@ -26,3 +31,4 @@ class Solution {
         return sb.toString();
     }
 }
+
