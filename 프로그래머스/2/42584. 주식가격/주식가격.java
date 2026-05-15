f742b3b25516fc5620b460a7c2fcb89035269317
Hyeoniill <52920187+hyeoniill@users.noreply.github.com>
2026-05-15 09:48:54 +0900

    [level 2] Title: 주식가격, Time: 11.87 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Deque<Integer> stack = new ArrayDeque<>();
-        
-        for(int i=0; i<prices.length; i++) {
-            while(!stack.isEmpty()) {
-                int j=stack.peek();
-                if(prices[i]<prices[j]) {
-                    answer[j] = i-j;
-                    stack.pop();
-                }
-                else {
-                    break;
-                }
-            }
-            stack.push(i);
-        }
-        
-        while(!stack.isEmpty()) {
-            int i = stack.pop();
-            answer[i] = prices.length-i-1;
-        }
+        int[] answer = {};
         return answer;
     }
 }
